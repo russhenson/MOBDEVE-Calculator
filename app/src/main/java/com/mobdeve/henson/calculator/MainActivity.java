b4c7da36a368556a57f80fd40daacaa54319af28
russhenson <49965783+russhenson@users.noreply.github.com>
2021-03-18 22:02:39 +0800

    hide title bar

--- app/src/main/java/com/mobdeve/henson/calculator/MainActivity.java
+++ app/src/main/java/com/mobdeve/henson/calculator/MainActivity.java
@@ -3,12 +3,17 @@ package com.mobdeve.henson.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // hide title in title bar
+        getSupportActionBar().hide(); // hide title bar
+
         setContentView(R.layout.activity_main);
     }
 }
