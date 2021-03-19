02d8af76d3ed7bfb82a547be6c8796f0d47e3fb5
russhenson <49965783+russhenson@users.noreply.github.com>
2021-03-19 22:43:23 +0800

    button functions on process

--- app/src/main/java/com/mobdeve/henson/calculator/MainActivity.java
+++ app/src/main/java/com/mobdeve/henson/calculator/MainActivity.java
@@ -10,11 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView titleTextView;
     private EditText computeEditText;
-    private Button opsBtn;
+    private Button opsBtn, btn1, btn2, btn3, btn4;
+    private Button btn5, btn6, btn7, btn8, btn9, btn0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +27,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.opsBtn = findViewById(R.id.opsBtn);
-        this.opsBtn.setOnClickListener(new View.OnClickListener() {
+        opsBtn.setOnClickListener(this);
+        this.btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+        this.btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(this);
+        this.btn3 = findViewById(R.id.btn3);
+        btn3.setOnClickListener(this);
+        this.btn4 = findViewById(R.id.btn4);
+        btn4.setOnClickListener(this);
+        this.btn5 = findViewById(R.id.btn5);
+        btn5.setOnClickListener(this);
+        this.btn6 = findViewById(R.id.btn6);
+        btn6.setOnClickListener(this);
+        this.btn7 = findViewById(R.id.btn7);
+        btn7.setOnClickListener(this);
+        this.btn8 = findViewById(R.id.btn8);
+        btn8.setOnClickListener(this);
+        this.btn9 = findViewById(R.id.btn9);
+        btn9.setOnClickListener(this);
+        this.btn0 = findViewById(R.id.btn0);
+        btn0.setOnClickListener(this);
+
+        /*this.opsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, OperationsActivity.class);
                 startActivity(i);
             }
-        });
+        });*/
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.opsBtn:
+                Intent i = new Intent(MainActivity.this, OperationsActivity.class);
+                startActivity(i);
+                break;
+            case R.id.btn1:
 
+        }
 
     }
+
 }
